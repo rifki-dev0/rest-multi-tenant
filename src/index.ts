3f55d2cc9d619d1d6db2d802a17753d12c1b2ddd
rifki-dev0 <rifki.dev0@gmail.com>
2025-02-18 14:45:10 +0700

    removing vercel related changes
    Signed-off-by: rifki-dev0 <rifki.dev0@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import "tsconfig-paths/register";
-import { app, httpServer } from "@/libs/server/graph-server";
+import { httpServer } from "@/libs/server/graph-server";
 import config from "@/config";
 
 httpServer.listen({ port: config.server.port });
 console.log(`Server is running on http://localhost:${config.server.port}`);
-export default app;
