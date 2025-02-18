e6bb5fb58528e41968f366ec08c9224634712ae6
rifki-dev0 <rifki.dev0@gmail.com>
2025-02-18 13:07:54 +0700

    configure new entry point for vercel deployment
    Signed-off-by: rifki-dev0 <rifki.dev0@gmail.com>

---
+++ api/index.ts
@@ -0,0 +1,6 @@
+// Import the 'express' module
+import { httpServer } from "@/libs/server/graph-server";
+import config from "@/config";
+
+httpServer.listen({ port: config.server.port });
+console.log(`Server is running on http://localhost:${config.server.port}`);
